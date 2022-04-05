110e6636a74a0f9cacba0291321798f6c54e2b08
Scott Turro <turroscott@gmail.com>
2022-04-04 22:06:59 -0500

    Changed graph to be on one plot

--- hw5/ensure_convergence.py
+++ hw5/ensure_convergence.py
@@ -9,43 +9,39 @@ if __name__ == "__main__":
     sim_count = 0
 
 
-    avg_cars_parked = list()
-    frac_balked = list()
+    avg_cars_parked_norm = list()
+    frac_balked_norm = list()
     for total_run_time in simulation_times:
         s = Simulation(total_run_time,rng_seed=simulation_seed)
 
-        avg_cars_parked.append(s.get_avg_num_current_parked())
-        frac_balked.append(s.get_fraction_cars_balked())
+        avg_cars_parked_norm.append(s.get_avg_num_current_parked())
+        frac_balked_norm.append(s.get_fraction_cars_balked())
         print(f"Simulation {sim_count+1}/{len(simulation_times)*2} Done.")
         sim_count += 1
 
-    plt.subplot(1,2,1)
-    plt.plot(simulation_times,avg_cars_parked,label="Normal Arrival Rate")
-    plt.xlabel("Simulation Time")
-    plt.ylabel("avg_cars_parked")
-    plt.subplot(1,2,2)
-    plt.plot(simulation_times,frac_balked,label="Normal Arrival Rate")
-    plt.xlabel("Simulation Time")
-    plt.ylabel("frac_balked")
-    plt.tight_layout()
-    plt.show()
-
-
-    avg_cars_parked = list()
-    frac_balked = list()
+    avg_cars_parked_inhomo = list()
+    frac_balked_inhomo = list()
     for total_run_time in simulation_times:
         s = Simulation(total_run_time,rng_seed=simulation_seed,increased_arrival_rate=12)
 
-        avg_cars_parked.append(s.get_avg_num_current_parked())
-        frac_balked.append(s.get_fraction_cars_balked())
+        avg_cars_parked_inhomo.append(s.get_avg_num_current_parked())
+        frac_balked_inhomo.append(s.get_fraction_cars_balked())
         print(f"Simulation {sim_count+1}/{len(simulation_times)*2} Done.")
         sim_count += 1
 
+
     plt.subplot(1,2,1)
-    plt.plot(simulation_times,avg_cars_parked,label="InHomo Arrival Rate")
+    plt.plot(simulation_times,avg_cars_parked_norm,label="Normal Arrival Rate")
+    plt.plot(simulation_times,avg_cars_parked_inhomo,label="InHomo Arrival Rate")
     plt.legend()
+    plt.xlabel("Simulation Time")
+    plt.ylabel("avg_cars_parked")
+
     plt.subplot(1,2,2)
-    plt.plot(simulation_times,frac_balked,label="InHomo Arrival Rate")
+    plt.plot(simulation_times,frac_balked_norm,label="Normal Arrival Rate")
+    plt.plot(simulation_times,frac_balked_inhomo,label="InHomo Arrival Rate")
     plt.legend()
+    plt.xlabel("Simulation Time")
+    plt.ylabel("frac_balked")
     plt.tight_layout()
     plt.show()
